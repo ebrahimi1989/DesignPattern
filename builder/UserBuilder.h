adf67d0a4b74263304b07884c3d22f10da0b6a73
Mohammad Ebrahimi <mohammad_ebrahimi22@hotmail.com>
2022-04-11 13:07:18 +0430

    20220411

---
+++ builder/UserBuilder.h
@@ -0,0 +1,31 @@
+#ifndef USERBUILDER_H
+#define USERBUILDER_H
+#include "User.h"
+
+class UserBuilder
+{
+public:
+    User *usr;
+    UserBuilder(int id);
+
+    const QString &getName() const;
+    void setName(const QString &newName);
+
+    const QString &getFamily() const;
+    void setFamily(const QString &newFamily);
+
+    int getTell() const;
+    void setTell(int newTell);
+
+    void setAge(int age);
+    int getAge();
+
+    QString getAddress();
+    void setAddress(QString address);
+
+    const QString &getPostalCode() const;
+    void setPostalCode(const QString &newPostalCode);
+
+};
+
+#endif // USERBUILDER_H
